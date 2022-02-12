915b22c989923e09db98f58612a71268fa1a8794
Josh Lee <jleedev@gmail.com>
2022-02-12 15:52:56 -0500

    Add more kinds of links

--- popup.js
+++ popup.js
@@ -1,6 +1,30 @@
-const {zoom, lat, lng} = JSON.parse(decodeURIComponent(location.hash.replace('#','')));
-let a = document.createElement('a');
-a.href = `https://www.openstreetmap.org/edit#map=${zoom}/${lat}/${lng}`;
-a.textContent = 'Edit on OpenStreetMap';
-a.target = '_blank';
-document.body.append(a);
+const { zoom, lat, lng } = JSON.parse(
+  decodeURIComponent(location.hash.replace("#", ""))
+);
+
+[
+  [
+    `https://www.openstreetmap.org/#map=${zoom}/${lat}/${lng}`,
+    'View on OpenStreetMap',
+  ],
+  [
+    `https://www.openstreetmap.org/edit#map=${zoom}/${lat}/${lng}`,
+    "Edit on OpenStreetMap",
+  ],
+  [
+    `https://wikishootme.toolforge.org/#lat=${lat}&lng=${lng}&zoom=${zoom}`,
+    "WikiShootMe!",
+  ],
+]
+  .map(([href, textContent]) =>
+    Object.assign(document.createElement("a"), {
+      href,
+      textContent,
+      target: "_blank",
+    })
+  )
+  .forEach((a) => {
+    const p = document.createElement('p');
+    p.append(a);
+    document.body.append(p);
+  });
